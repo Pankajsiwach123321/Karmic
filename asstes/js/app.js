73946b21ec5e2eca31f9ca6129ae80a7107aaf02
Pankajsiwach123321 <pankusiwach000@gmai.com>
2023-09-05 16:15:24 +0530

    first commit

---
+++ asstes/js/app.js
@@ -0,0 +1,5 @@
+let nav = document.querySelector(".nav");
+let menubar = document.querySelector(".menubar");
+nav.addEventListener("click", function () {
+    menubar.classList.toggle("right-0")
+})
