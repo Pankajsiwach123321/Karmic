ef8584e0ffb8093b0f479d245d0ae423bb1dd93c
Pankajsiwach123321 <pankusiwach000@gmai.com>
2023-09-11 16:02:12 +0530

    block line on media querry or change js function

--- asstes/js/app.js
+++ asstes/js/app.js
@@ -1,11 +1,14 @@
 let nav = document.querySelector(".nav");
 let menubar = document.querySelector(".menubar");
+let finish = document.querySelectorAll(".finish");
 let bgbody = document.querySelector(".bg-body");
 nav.addEventListener("click", function () {
     menubar.classList.toggle("right-0")
     bgbody.classList.toggle("over-flow")
 })
-menubar.addEventListener("click", function () {
-    menubar.classList.toggle("right-0")
-    bgbody.classList.remove("over-flow")
-})
+finish.forEach(e => {
+    e.addEventListener("click", function () {
+        menubar.classList.remove("right-0")
+        bgbody.classList.remove("over-flow")
+    })
+});
